af409f842871b9a304140e1ae6d9218260699c9e
LouisVie61 <tantrieucao@gmail.com>
2025-02-28 13:37:50 +0700

    feat: adding the new Chatbot with new aspect

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
-from API.chatbot_logic import chat_response
+from API.chatbot_logic import chat_response, chat_response_ver2
 from API.textsummarization_logic import summarize_text
 from API.upload_routes import upload_router
+from sqlalchemy.orm import Session
+from Config.database import get_db
 
 # Run: uvicorn Main:app --reload
 app = FastAPI()
@@ -18,6 +20,11 @@ class Summarization_Request(BaseModel):
 @app.post("/chatbot/")
 async def chatbot(request: Chat_Request):
     response = chat_response(request.user_input)
+    return {"response": response}
+
+@app.post("/chatbot_2/")
+async def chatbot_2(request: Chat_Request, db: Session = Depends(get_db)):
+    response = chat_response_ver2(request.user_input, db)
     return {"response": response}
 
 @app.post("/summarize/")
